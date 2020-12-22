867984957dbd3e29ca7c31fcdc754176ff976c6e
Cristobal Lopez <crolopezp@gmail.com>
2020-12-22 10:47:13 -0800

    Remove unnecessary middlewares

--- src/models/Whitelist.ts
+++ src/models/Whitelist.ts
@@ -1,13 +1,6 @@
 import mongoose from 'mongoose'
 import { WhitelistSchema } from './schemas/WhitelistSchema'
-import { WhiteListMiddleware } from './middlewares/whitelist'
 import { UserDoc } from '../types/UserDoc'
-
-// Middlewares
-WhitelistSchema.post('save', WhiteListMiddleware.WhiteListPostSaveMiddleware)
-WhitelistSchema.post('findOneAndUpdate', function(result) {
-  console.log('===postfindOneAndUpdate===%s==', result)
-});
 
 // The model
 export default mongoose.model<UserDoc>('Whitelist', WhitelistSchema)
