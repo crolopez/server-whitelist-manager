0a61f32fa608fc051198614b608661bf43331e4f
Cristobal Lopez <crolopezp@gmail.com>
2021-01-04 04:03:26 -0800

    Add unit tests for TimeAccessHandler class

--- tests/unit/spec/main.spec.ts
+++ tests/unit/spec/main.spec.ts
@@ -1,6 +1,8 @@
 import nock from 'nock'
+import moment from 'moment'
 import UUIDHandler from '../../../src/helpers/UUIDHandler'
 import HttpClient from '../../../src/helpers/HttpClient'
+import TimeAccessHandler from '../../../src/helpers/TimeAccessHandler'
 
 describe('Whitelist manager', () => {
   beforeEach(() => {
@@ -34,6 +36,7 @@ describe('Whitelist manager', () => {
     beforeEach(() => {
       nock.disableNetConnect()
     })
+
     test('#get', async () => {
       // data
       const url = 'https://fake.com'
@@ -85,6 +88,94 @@ describe('Whitelist manager', () => {
       const headersNoToken = { headers: {} }
 
       expect(HttpClient.buildHeaders(undefined)).toEqual(headersNoToken)
+    })
+  })
+
+  describe('Class TimeAccessHandler', () => {
+    let BaseDate: moment.Moment
+    beforeEach(() => {
+      BaseDate = moment('2021-01-01T00:00:00.000Z')
+      Date.now = jest.fn(() => BaseDate.valueOf())
+
+    })
+
+    test('#increaseTime (with seconds)', async () => {
+      const Increment = '1s'
+      const ExpectedDate = new Date('2021-01-01T00:00:01.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#increaseTime (with minutes)', async () => {
+      const Increment = '1m'
+      const ExpectedDate = new Date('2021-01-01T00:01:00.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#increaseTime (with hours)', async () => {
+      const Increment = '1h'
+      const ExpectedDate = new Date('2021-01-01T01:00:00.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#increaseTime (with days)', async () => {
+      const Increment = '1d'
+      const ExpectedDate = new Date('2021-01-02T00:00:00.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#increaseTime (with weeks)', async () => {
+      const Increment = '1w'
+      const ExpectedDate = new Date('2021-01-08T00:00:00.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#increaseTime (with months)', async () => {
+      const Increment = '1M'
+      const ExpectedDate = new Date('2021-02-01T00:00:00.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#increaseTime (without suffix)', async () => {
+      const Increment = '1'
+      const ExpectedDate = new Date('2021-01-01T00:00:01.000Z')
+
+      const result = await TimeAccessHandler.increaseTime(BaseDate, Increment)
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#getNewExpiryDate', async () => {
+      const ExpectedDate = new Date('2021-02-01T00:00:00.000Z')
+
+      const result = await TimeAccessHandler.getNewExpiryDate('1M')
+
+      expect(result).toStrictEqual(ExpectedDate)
+    })
+
+    test('#getUpdatedExpiryDate', async () => {
+      const ExpectedDate = new Date('2021-01-11T00:00:00.000Z')
+
+      const result = await TimeAccessHandler.getUpdatedExpiryDate(BaseDate.toDate(), '10d')
+
+      expect(result).toStrictEqual(ExpectedDate)
     })
   })
 })
