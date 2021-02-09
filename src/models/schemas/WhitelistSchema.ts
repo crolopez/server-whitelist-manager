4c98d7fc19d8236582231e9377d1af3d02428a71
Cristobal Lopez <crolopezp@gmail.com>
2021-02-09 10:33:34 -0800

    Make optional the twitch_user field in body requests  (#6)
    * Add auto-trim to userDoc strings
    
    * Make optional the twitch_user field in body requests
    
    * Up module version

--- src/models/schemas/WhitelistSchema.ts
+++ src/models/schemas/WhitelistSchema.ts
@@ -2,9 +2,9 @@
 import { Schema } from 'mongoose'
 
 const WhitelistSchema = new Schema({
-  game_tag: { type: String, unique: true },
-  twitch_user: { type: String, unique: true },
-  note: String,
+  game_tag: { type: String, trim: true, unique: true, sparse: true },
+  twitch_user: { type: String, trim: true, unique: true, sparse: true },
+  note: { type: String, trim: true},
   uuid: String,
   access_expiry_date: Date,
 }, {
--- src/types/UserDoc.ts
+++ src/types/UserDoc.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 export interface UserDoc extends mongoose.Document {
-  game_tag: { type: string, unique: true },
-  twitch_user: { type: string, unique: true },
-  note: string,
+  game_tag: { type: string, unique: true, sparse: true },
+  twitch_user: { type: string, unique: true, sparse: true },
+  note: { type: string, trim: true},
   uuid: string,
   access_expiry_date: Date,
 }
