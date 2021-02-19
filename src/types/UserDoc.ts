29d2d8227071c0403d7ed577181c294f81f99238
Cristobal Lopez <crolopezp@gmail.com>
2021-02-19 06:35:39 -0800

    Remove unique index from twitch and discord fields
    CosmosDB doesn't support sparse index

--- src/types/UserDoc.ts
+++ src/types/UserDoc.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 export interface UserDoc extends mongoose.Document {
   game_tag: { type: string, unique: true, sparse: true },
-  twitch_user: { type: string, unique: true, sparse: true },
-  discord_user: { type: string, unique: true, sparse: true },
+  twitch_user: { type: string },
+  discord_user: { type: string },
   note: { type: string, trim: true},
   uuid: string,
   access_expiry_date: Date,
